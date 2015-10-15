e76e1452348b495094b95e548edf68cf65ad4ad1
carlosgarijo <carlos.garijo.gyg@gmail.com>
2015-10-15 17:09:03 +0200

    Parser y cojo tags

--- karaoke.py
+++ karaoke.py
@@ -10,8 +10,16 @@ if __name__ == "__main__":
     """
     Programa principal
     """
-
+    fich = sys.argv[1]
     try:
-        fich = open(sys.argv[1])
+        fichero = open(fich)
     except IndexError:
         print("Usage: python3 karaoke.py file.smil.")
+
+    parser = make_parser()
+    ssHandler = smallsmilhandler.SmallSMILHandler()
+    parser.setContentHandler(ssHandler)
+    parser.parse(fichero)
+
+    tags = ssHandler.get_tags()
+    print(tags)
