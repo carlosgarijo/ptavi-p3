1adbc420d98024dcea7af72dce20b91158055a83
carlosgarijo <carlos.garijo.gyg@gmail.com>
2015-10-15 18:53:04 +0200

    Con Urllib no acabado

--- karaoke.py
+++ karaoke.py
@@ -5,6 +5,8 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 import smallsmilhandler
 import sys
+import json
+import urllib.request
 
 
 def listado_ordenado(tags):
@@ -16,6 +18,19 @@ def listado_ordenado(tags):
                 atrib_line = atrib_line + key + "=" + diccs[key] + "\t"
         listado = listado + diccs["name"] + "\t" + atrib_line + "\n"
     return listado
+
+def url_local(tags):
+    list_url = []
+    for diccs in tags:
+        for key in diccs.keys():
+            if key == "src":
+                url = diccs[key]
+                arch_web = urllib.request.urlopen(url)
+                list_url = url.split("/")
+                filename = list_url[-1]
+                diccs[key] = filename
+    f = open(filename, "wb")
+    f.write(arch_web.read())
 
 if __name__ == "__main__":
 
@@ -33,3 +48,8 @@ if __name__ == "__main__":
     tags = ssHandler.get_tags()
     result = listado_ordenado(tags)
     print(result)
+    """json_listado = json.dumps(result)
+    print(json_listado)
+    with open('/home/garijos/Escritorio/PTAVI/ptavi-p3/karaoke.json', 'w') as outfile:
+        json.dump(tags, outfile)"""
+    download = url_local(tags)
