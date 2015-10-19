588ac0b7b969477459995315bae8c76a6fe900f6
carlosgarijo <carlos.garijo.gyg@gmail.com>
2015-10-19 10:16:05 +0200

    Mejorado con un solo Diccionario

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -9,44 +9,21 @@ import sys
 class SmallSMILHandler(ContentHandler):
 
     def __init__(self):
-        self.root_layout = ['width', 'height', 'background-color']
-        self.region = ['id', 'top', 'bottom', 'left', 'right']
-        self.img = ['src', 'region', 'begin', 'dur']
-        self.audio = ['src', 'begin', 'dur']
-        self.textstream = ['src', 'region']
+        self.list_diccs = {'root-layout': ['width', 'height', 'background-color'],
+                           'region': ['id', 'top', 'bottom', 'left', 'right'],
+                           'img': ['src', 'region', 'begin', 'dur'],
+                           'audio': ['src', 'begin', 'dur'],
+                           'textstream': ['src', 'region']}
+
         self.tag = []
 
     def startElement(self, name, attrs):
 
-        if name == 'root-layout':
-            dicc = {"name": "Root-Layout"}
-            for atributo in self.root_layout:
+        if name in self.list_diccs:
+            dicc = {}
+            for atributo in self.list_diccs[name]:
                 dicc[atributo] = attrs.get(atributo, "")
-            self.tag.append(dicc)
-
-        elif name == 'region':
-            dicc = {"name": "Region"}
-            for atributo in self.region:
-                dicc[atributo] = attrs.get(atributo, "")
-            self.tag.append(dicc)
-
-        elif name == 'img':
-            dicc = {"name": "Img"}
-            for atributo in self.img:
-                dicc[atributo] = attrs.get(atributo, "")
-            self.tag.append(dicc)
-
-        elif name == 'audio':
-            dicc = {"name": "Audio"}
-            for atributo in self.audio:
-                dicc[atributo] = attrs.get(atributo, "")
-            self.tag.append(dicc)
-
-        elif name == 'textstream':
-            dicc = {"name": "Textstream"}
-            for atributo in self.textstream:
-                dicc[atributo] = attrs.get(atributo, "")
-            self.tag.append(dicc)
+            self.tag.append([name, dicc])
 
     def get_tags(self):
         return self.tag
