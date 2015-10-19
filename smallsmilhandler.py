bf39d86881bdfa90890867f8cb57ffb3b19bbbfd
carlosgarijo <carlos.garijo.gyg@gmail.com>
2015-10-19 16:14:44 +0200

    finalizado y pasado pep8

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -9,21 +9,21 @@ import sys
 class SmallSMILHandler(ContentHandler):
 
     def __init__(self):
-        self.list_diccs = {'root-layout': ['width', 'height', 'background-color'],
-                           'region': ['id', 'top', 'bottom', 'left', 'right'],
-                           'img': ['src', 'region', 'begin', 'dur'],
-                           'audio': ['src', 'begin', 'dur'],
-                           'textstream': ['src', 'region']}
+        self.diccs = {'root-layout': ['width', 'height', 'background-color'],
+                      'region': ['id', 'top', 'bottom', 'left', 'right'],
+                      'img': ['src', 'region', 'begin', 'dur'],
+                      'audio': ['src', 'begin', 'dur'],
+                      'textstream': ['src', 'region']}
 
         self.tag = []
 
     def startElement(self, name, attrs):
 
-        if name in self.list_diccs:
-            dicc = {}
-            for atributo in self.list_diccs[name]:
-                dicc[atributo] = attrs.get(atributo, "")
-            self.tag.append([name, dicc])
+        if name in self.diccs:
+            help_dicc = {}
+            for atributo in self.diccs[name]:
+                help_dicc[atributo] = attrs.get(atributo, "")
+            self.tag.append([name, help_dicc])
 
     def get_tags(self):
         return self.tag
